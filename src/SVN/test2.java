d2e89ba80b0195f1884f6b729d3db9f668fb5409
spawar20 <sainathpawar20@gmail.com>
2018-10-07 13:32:33 +0530

    webdrivercode

--- src/SVN/test2.java
+++ src/SVN/test2.java
@@ -1,10 +1,16 @@
 package SVN;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Its been changed");
+		System.out.println("Its been changed");
+		System.setProperty("Webdriver.chrome.driver", "F:/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.cricbuzz.com");
 	}
 
 }
