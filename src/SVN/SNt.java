d8e3c1078946ab368cee139fa7317e452892107a
spawar20 <43914793+spawar20@users.noreply.github.com>
2018-10-07 13:40:27 +0530

    Update SNt.java

--- src/SVN/SNt.java
+++ src/SVN/SNt.java
@@ -4,7 +4,8 @@ public class SNt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hi");
+		System.out.println("Its been changed");
+		System.setProperty("Webdriver.chrome.driver", "F:/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.cricbuzz.com");
 	}
-
-}
